7595b8105141783cc930b089bf56da1d38b68d6e
Simon Richard-Giroux <simon.richard-giroux.1@ens.etsmtl.ca>
2017-02-05 10:21:22 -0500

    User login

--- WebFlix/WebFlix/FormLogin.cs
+++ WebFlix/WebFlix/FormLogin.cs
@@ -18,21 +18,38 @@ namespace WebFlix
         public FormLogin()
         {
             InitializeComponent();
+
+            // Pour accélérer le développement, valeurs par défaut au login.
+            textBoxEmail.Text = "RobertCFlores21@gmail.com";
+            textBoxPassword.Text = "eishie3meiH";
         }
 
         private void buttonLogin_Click(object sender, EventArgs e)
         {
             ISession s = NHibernateHelper.GetCurrentSession();
             ITransaction tx = s.BeginTransaction();
-            IQuery query = s.CreateQuery("select u from Users u where u.Courriel = :courriel");
-            query.SetString("courriel", "RobertCFlores21@gmail.com");
-
+            IQuery query = s.CreateQuery("select u from Users u where u.Courriel = :courriel and u.Motpasse = :motpasse");
+            query.SetString("courriel", textBoxEmail.Text);
+            query.SetString("motpasse", textBoxPassword.Text);
             IList<Users> result = query.List<Users>();
-            foreach (Users u in result)
-            {
-                Console.Out.WriteLine(u.Nom);
-            }
             tx.Commit();
+
+            if (result.Count > 0)
+            {
+                // Enregistre l'utilisateur courant dans un singleton
+                UserSession.Current = result.First<Users>();
+
+                // Navigue vers la page de 'browsing'
+                var th = new Thread(() => Application.Run(new FormBrowse()));
+                th.SetApartmentState(ApartmentState.STA); // Deprecation Fix
+                th.Start();
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Courriel ou mot de passe invalide");
+            }
         }
     }
 }
